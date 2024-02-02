db7c4606ef9f30566e1d9399d737dc6a1b17b967
heejin1231 <honghijin@gmail.com>
2024-02-03 02:42:51 +0800

    This is a P2 QUIZ in ITE393

---
+++ script.js
@@ -0,0 +1,11 @@
+//Hong, Hijin
+//BSIT2-04
+//ITE 393
+//P2 QUIZ
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
